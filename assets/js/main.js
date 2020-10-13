c89cd61357f13e4d7782245ecd5d42b02818c6a6
cane21 <cane21@kookmin.ac.kr>
2020-10-13 17:51:23 +0900

    change update

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,26 +107,26 @@ window.onkeydown=function()
             rate: 0.8,
             pitch: 1.2,
             lang: "ko-KR"}
-        )
+        );
     }
 }
 
 function speak(text, opt_prop) {
     if (typeof SpeechSynthesisUtterance === "undefined" || typeof window.speechSynthesis === "undefined") {
-        alert("이 브라우저는 음성 합성을 지원하지 않습니다.")
-        return
+        alert("이 브라우저는 음성 합성을 지원하지 않습니다.");
+        return;
     }
 
-    window.speechSynthesis.cancel() // 현재 읽고있다면 초기화
+    window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
 
-    const prop = opt_prop || {}
+    const prop = opt_prop || {};
 
-    const speechMsg = new SpeechSynthesisUtterance()
-    speechMsg.rate = prop.rate || 1 // 속도: 0.1 ~ 10
-    speechMsg.pitch = prop.pitch || 1 // 음높이: 0 ~ 2
-    speechMsg.lang = prop.lang || "ko-KR"
-    speechMsg.text = text
+    const speechMsg = new SpeechSynthesisUtterance();
+    speechMsg.rate = prop.rate || 1; // 속도: 0.1 ~ 10
+    speechMsg.pitch = prop.pitch || 1; // 음높이: 0 ~ 2
+    speechMsg.lang = prop.lang ||; "ko-KR"
+    speechMsg.text = text;
 
     // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-    window.speechSynthesis.speak(speechMsg)
+    window.speechSynthesis.speak(speechMsg);
 }
