0d618de2639c2d095de85d64bc92d2a029d273db
cane21 <cane21@kookmin.ac.kr>
2020-11-03 04:51:57 +0900

    Update sub.js

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -52,19 +52,16 @@ window.addEventListener('load', async function()
         }
         timeArray[i].innerHTML = Weekday[i];
 
-        var temp= parseInt(weatherResponse.hourly[i].feels_like);
+        // var temp= parseInt(weatherResponse.hourly[i].feels_like);
+        var temp= weatherResponse.daily[i].uvi;
 
-        if(temp<10)
+        if(temp<5)
         {
-            emotionImg[i].style.background="url(\"../assets/images/normal.png\") no-repeat 50% 50%/100% 100%";
-        }
-        else if(temp<11)
-        {
-            emotionImg[i].style.background="url(\"../assets/images/normal.png\") no-repeat 50% 50%/100% 100%";
+            emotionImg[i].style.background="url(\"../assets/images/smile.png\") no-repeat 50% 50%/100% 100%";
         }
         else
         {
-            emotionImg[i].style.background="url(\"../assets/images/normal.png\") no-repeat 50% 50%/75% 75%";
+            emotionImg[i].style.background="url(\"../assets/images/bad.png\") no-repeat 50% 50%/75% 75%";
         }
 
         // weatherImg[i].setAttribute('src',imgName[weatherResponse.hourly[count].weather[0].main]);
@@ -74,16 +71,19 @@ window.addEventListener('load', async function()
     for(var i=0 ; i< 7; i++)
     {
         // t[i].innerHTML = parseInt(Math.round(weatherResponse.daily[i].temp.day));
-        t[i].innerHTML = weatherResponse.hourly[i].feels_like;
+        // t[i].innerHTML = weatherResponse.hourly[i].feels_like;
+        t[i].innerHTML = weatherResponse.daily[i].uvi;
 
-        var temp= parseInt(weatherResponse.hourly[i].feels_like);
+        // var temp= parseInt(weatherResponse.daily[i].uvi);
+        var temp= weatherResponse.daily[i].uvi;
 
-        if(temp<10)
+        // console.log(temp);
+        if(temp<2)
         {
-            windArray[i].innerHTML = "나쁨";
+            windArray[i].innerHTML = "낮음";
             // emotionImg[i].setAttribute('src','../assets/images/normal.png')
         }
-        else if(temp<11)
+        else if(temp<5)
         {
             windArray[i].innerHTML = "보통";
             // emotionImg[i].setAttribute('src','../assets/images/normal.png')
@@ -97,18 +97,18 @@ window.addEventListener('load', async function()
         // weatherImg[i+5].setAttribute('src',imgName[weatherResponse.daily[i].weather[0].main]);
     }
 
-    if(weatherResponse.hourly[0].feels_like<10)
+    if(weatherResponse.daily[0].uvi<2)
     {
         document.getElementById("bar").setAttribute('value',weatherResponse.hourly[0].feels_like*5.5);
         document.getElementsByClassName("sentence")[0].children[0].innerHTML="나쁨";
         document.getElementsByClassName("sentence")[0].children[1].innerHTML="되도록이면 외출을 삼가세요!";
     }
-    else if(weatherResponse.hourly[0].feels_like<11)
+    else if(weatherResponse.daily[0].uvi<5)
     {
         document.getElementsByClassName("sentence")[0].children[0].innerHTML="보통";
         document.getElementsByClassName("sentence")[0].children[1].innerHTML="지금은 외출하셔도 좋습니다";
     }
-    else if(weatherResponse.hourly[0].feels_like>11)
+    else
     {
         document.getElementsByClassName("sentence")[0].children[0].innerHTML="좋음";
         document.getElementsByClassName("sentence")[0].children[1].innerHTML="외출하기에 최적의 날씨입니다";
